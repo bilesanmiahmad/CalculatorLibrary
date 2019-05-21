0eee3e229db020582f0ae73ed48331db0d323c16
Bilesanmi Ahmad <fbilesanmi@gmail.com>
2019-05-21 13:31:22 +0000

    added code for division and its tests

--- calculator.py
+++ calculator.py
@@ -9,3 +9,10 @@ def subtract(num1, num2):
 
 def multiply(num1, num2):
     return num1 * num2
+
+
+def divide(num1, num2):
+    try:
+        return num1 / num2
+    except ZeroDivisionError:
+        print("Denominator cannot be zero")
--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,8 @@
 import calculator
+import unittest2 as unittest
 
 
-class TestCalculator:
+class TestCalculator(unittest.TestCase):
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
 
@@ -10,3 +11,10 @@ class TestCalculator:
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
+    
+    def test_division(self):
+        assert 2 == calculator.divide(100, 50)
+
+    def test_division_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            calculator.divide(20, 0)
